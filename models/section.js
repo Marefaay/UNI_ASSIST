a77721afccf225901039b4fcd41826819ac345d4
Mahmoud Refaay <125451007+Marefaay@users.noreply.github.com>
2024-06-15 03:58:19 +0300

    Update section.js

--- models/section.js
+++ models/section.js
@@ -1,43 +1,27 @@
-const mongoose =require('mongoose')
+const mongoose = require("mongoose");
 
 // Define section schema
 
-  const sectionSchema = new mongoose.Schema({
-    title: {
-      type: String,
-      minlength: 3,
-      maxlength: 25,
-      trim: true,
-
-      required: true,
+const sectionSchema = new mongoose.Schema({
+  number: {
+    type: Number,
+    required: true,
+  },
+  subject: {
+    type: mongoose.Types.ObjectId,
+    ref: "Subject",
+  },
+  addedBy: {
+    type: mongoose.Types.ObjectId,
+    ref: "prof-profAssist",
+  },
+  fileUrl: {
+    type: Object,
+    default: {
+      url: "https://th.bing.com/th/id/R.152c34a899b6bf22d4da6c91b74403dd?rik=ELN9t4jt5Z7dhA&pid=ImgRaw&r=0",
+      publidId: null,
     },
-    ID: {
-      type: String,
-      minlength: 4,
-      maxlength: 5,
-      unique: true,
-      required: true,
-    },
-    numberOfHours: {
-      type: Number,
-      required: true,
-    },
-     
-    addedBy: {
-        type: [mongoose.Types.ObjectId],
-        // required: true,
-  
-        ref: "prof-profAssist",
-      },
-    fileUrl: {
-        type: Object,
-        default: {
-          url: "https://th.bing.com/th/id/R.152c34a899b6bf22d4da6c91b74403dd?rik=ELN9t4jt5Z7dhA&pid=ImgRaw&r=0",
-          publidId: null,
-        },
-      }, // Store Cloudinary URL
-  });
-  module.exports= mongoose.model('Section', sectionSchema)
-
-  
-  
+  }, // Store Cloudinary URL
+});
+const sectionModel = mongoose.model("Section", sectionSchema);
+module.exports = sectionModel;
