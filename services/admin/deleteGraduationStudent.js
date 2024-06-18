835e5a46a9edcd79592086fa4ae70bea7dbab868
Mahmoud Refaay <125451007+Marefaay@users.noreply.github.com>
2024-06-18 19:53:23 +0300

    Update deleteGraduationStudent.js

--- services/admin/deleteGraduationStudent.js
+++ services/admin/deleteGraduationStudent.js
@@ -3,27 +3,27 @@ const gradautionStudentModel = require("../../models/gaduationStudent");
 const removeFromCloudinary = require("../../utils/removeFromCloudinary");
 
 const deleteGrduationStudent = async (request, response) => {
-  const { id } = request.params;
+  const { email } = request.body;
   //find student
-  const student = await gradautionStudentModel.findOne({ _id: id });
+  const student = await gradautionStudentModel.findOne({ email });
   if (student) {
     //if student exist
     //delte Studnet
-    await gradautionStudentModel.deleteOne({ _id: id });
+    await gradautionStudentModel.deleteOne({ email });
     //delete profile photo
     await removeFromCloudinary(student.profilePhoto.publicId);
     //delete posts comments@@@TODO
     //response
     return response.json({
       status: "Success",
-      message: "Graduation Student Deleted Succefully",
+      message: `Graduation Student with email('${email}') Deleted Succefully`,
     });
   } else {
     //If STudent Not Exist
     //response
     return response.json({
       status: "Error",
-      message: `No Graduation Student With this ID ('${id}')`,
+      message: `No Graduation Student With this ID ('${email}')`,
     });
   }
 };
