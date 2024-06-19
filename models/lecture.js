b02dcbf14d1a71e951b4c2bffcdff9ddc4827eed
Mahmoud Refaay <125451007+Marefaay@users.noreply.github.com>
2024-06-19 23:07:14 +0300

    Update lecture.js

--- models/lecture.js
+++ models/lecture.js
@@ -21,7 +21,8 @@ const lectureSchema = new mongoose.Schema({
       url: "https://th.bing.com/th/id/R.152c34a899b6bf22d4da6c91b74403dd?rik=ELN9t4jt5Z7dhA&pid=ImgRaw&r=0",
       publidId: null,
     },
-  }, // Store Cloudinary URL
+  },
+  generatedAt: String, // Store Cloudinary URL
 });
 const lectureModel = mongoose.model("Lecture", lectureSchema);
 module.exports = lectureModel;
