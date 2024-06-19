1c029a14cea2690b28626add16f4feffa4484ab1
Mahmoud Refaay <125451007+Marefaay@users.noreply.github.com>
2024-06-19 23:06:40 +0300

    Update section.js

--- models/section.js
+++ models/section.js
@@ -21,7 +21,9 @@ const sectionSchema = new mongoose.Schema({
       url: "https://th.bing.com/th/id/R.152c34a899b6bf22d4da6c91b74403dd?rik=ELN9t4jt5Z7dhA&pid=ImgRaw&r=0",
       publidId: null,
     },
-  }, // Store Cloudinary URL
+
+  },
+  generatedAt: String, // Store Cloudinary URL
 });
 const sectionModel = mongoose.model("Section", sectionSchema);
 module.exports = sectionModel;
