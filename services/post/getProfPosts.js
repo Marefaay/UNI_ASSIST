e1c5dcfda667f61a2378a27f4c4da8348f52322c
Mahmoud Refaay <125451007+Marefaay@users.noreply.github.com>
2024-06-20 00:45:31 +0300

    Update getProfPosts.js

--- services/post/getProfPosts.js
+++ services/post/getProfPosts.js
@@ -4,7 +4,20 @@ const getProfPosts = async (request, response) => {
   //find posts that published by prof
   const posts = await postModel
     .find({ publisher: request.id })
-    .populate("publisher");
+    .populate("publisher", {
+      _id: 0,
+      email: 0,
+      password: 0,
+      subject: 0,
+      profilePhoto: 0,
+      addedBy: 0,
+      role: 0,
+      isAdmin: 0,
+      __v: 0,
+      createdAt: 0,
+      updatedAt: 0,
+    });
+
   const postsCount = posts.length;
   console.log(posts.length);
   //if prof have no posts
