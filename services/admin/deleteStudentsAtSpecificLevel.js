067e08184725e08ac689c8abce8fac14a9ee4e08
Mahmoud Refaay <125451007+Marefaay@users.noreply.github.com>
2024-06-21 19:26:36 +0300

    Update deleteStudentsAtSpecificLevel.js

--- services/admin/deleteStudentsAtSpecificLevel.js
+++ services/admin/deleteStudentsAtSpecificLevel.js
@@ -20,6 +20,48 @@ const deleteStudentsAtSpecificLevel = async (request, response) => {
         message: `No Students In Level ${level}`,
       });
     }
+    //remove images from cloudniry
+    students.forEach(async (std) => {
+      await removeFromCloudinary(std.profilePhoto.publicId);
+    });
+    //delete comments
+    //student comments
+    const studentComments = await commentModel.find({ student: student._id });
+    console.log(studentComments);
+    //post comments
+    const posts = await postModel.find({});
+    posts.forEach(async (post) => {
+      console.log(post.comments);
+      studentComments.forEach(async (studentComment) => {
+        //check if student have a comment
+        const studentCommentExistInPOstComment = post.comments.includes(
+          studentComment._id
+        );
+        if (studentCommentExistInPOstComment) {
+          let indexOfStudentCommentInPostComments = post.comments.indexOf(
+            studentComment._id
+          );
+          if (indexOfStudentCommentInPostComments > -1) {
+            post.comments.splice(indexOfStudentCommentInPostComments, 1);
+          }
+        }
+      });
+      await commentModel.deleteMany({ student: student._id });
+
+      //check if student likes posts
+      const studentLikesPosts = post.likes.includes(student._id);
+      if (studentLikesPosts) {
+        //find index of student
+        let indexOfStudent = post.likes.indexOf(student._id);
+        // if found
+        if (indexOfStudent > -1) {
+          //remove student
+          post.likes.splice(indexOfStudent, 1);
+        }
+      }
+      await post.save();
+    });
+
     //student exist
     await studentModel.deleteMany({ level: level });
     //response
