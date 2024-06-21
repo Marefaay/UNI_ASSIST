4ece32e9c20d5d08b2b0300c6d0124f805299071
Mahmoud Refaay <125451007+Marefaay@users.noreply.github.com>
2024-06-21 19:12:59 +0300

    Update delteStudent.js

--- services/admin/delteStudent.js
+++ services/admin/delteStudent.js
@@ -1,4 +1,6 @@
 //OK
+const commentModel = require("../../models/comment");
+const postModel = require("../../models/post");
 const studentModel = require("../../models/student");
 const removeFromCloudinary = require("../../utils/removeFromCloudinary");
 
@@ -8,11 +10,48 @@ const deleteStudent = async (request, repsone) => {
   //find Student
   const student = await studentModel.findOne({ email });
   if (student) {
-    //delte student from database
-    await studentModel.deleteOne({ email });
     //delete profile photo
     await removeFromCloudinary(student.profilePhoto.publicId);
-    //delete comments @TODO
+    //delete comments
+    //student comments
+    const studentComments = await commentModel.find({ student: student._id });
+    console.log(studentComments);
+    //post comments
+    const posts = await postModel.find({});
+    posts.forEach(async (post) => {
+      console.log(post.comments);
+      studentComments.forEach(async (studentComment) => {
+        //check if student have a comment
+        const studentCommentExistInPOstComment = post.comments.includes(
+          studentComment._id
+        );
+        if (studentCommentExistInPOstComment) {
+          let indexOfStudentCommentInPostComments = post.comments.indexOf(
+            studentComment._id
+          );
+          if (indexOfStudentCommentInPostComments > -1) {
+            post.comments.splice(indexOfStudentCommentInPostComments, 1);
+          }
+        }
+      });
+      await commentModel.deleteMany({ student: student._id });
+
+      //check if student likes posts
+      const studentLikesPosts = post.likes.includes(student._id);
+      if (studentLikesPosts) {
+        //find index of student
+        let indexOfStudent = post.likes.indexOf(student._id);
+        // if found
+        if (indexOfStudent > -1) {
+          //remove student
+          post.likes.splice(indexOfStudent, 1);
+        }
+      }
+      await post.save();
+    });
+
+    //delte student from database
+    await studentModel.deleteOne({ email });
     //response
     return repsone.json({
       status: "Succes",
